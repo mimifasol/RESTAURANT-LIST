ef3c60af7c12968242a16493c6b4a4f63a632de8
mimifasol <mimifaso@gmail.com>
2023-04-15 16:21:22 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 // require packages used in the project
-const express = require('express')
-const app = express()
+const express = require('express') //載入express
+const app = express() //設定app為主入口
 const port = 3000
 const exphbs = require('express-handlebars') // require express-handlebars
+const mongoose = require('mongoose') //載入mongoose
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const restaurantsList = require('./restaurant.json') //載入restaurant.json
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true }) //設定連線到mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 // routes setting for index頁面
 app.get('/', (req, res) => {
